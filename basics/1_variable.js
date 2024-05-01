7d78d78d1a4713476c97f5cd76d5be8c05c2f90e
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-05-01 19:25:28 +0000

    datatypes

--- basics/1_variable.js
+++ basics/1_variable.js
@@ -1,7 +1,9 @@
 const id = 12217388
 let firstName = "bhupendra"
-var lastName = "gahlot"
+var lastName = "gahlot" // prefer not to use var because issue in block scope {} and functional scope
+let Contact 
 city = "Kota"
+
 
 // id = "1233458" not allow because const is not charged value
 
@@ -10,4 +12,4 @@ console.log(firstName);
 console.log(lastName);
 console.log(city);
 
-console.table([id,firstName,lastName,city]);
+console.table([id,firstName,lastName,city,Contact]);
---
+++ basics/2_dataType.js
@@ -0,0 +1,22 @@
+// alert("hello") //we are using node.js not browser
+
+let name = "bhupendra"
+let age = 22
+let login = false
+
+
+// dataTypes:
+
+// number = 2 to power 53
+// bigint = using for larger numbers 
+// string = "bhuependra"
+//Boolean = true/false / yas / no
+
+// Null = standalone value   // null typeof is object 
+// undefined = value not a sign 
+// symbol = unique 
+
+
+// objects:
+
+
