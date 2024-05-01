e6487577687d7b60afc53892a16bf83f42fb2ea1
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-05-01 18:43:07 +0000

    variable

---
+++ basics/1_variable.js
@@ -0,0 +1,13 @@
+const id = 12217388
+let firstName = "bhupendra"
+var lastName = "gahlot"
+city = "Kota"
+
+// id = "1233458" not allow because const is not charged value
+
+console.log(id);
+console.log(firstName);
+console.log(lastName);
+console.log(city);
+
+console.table([id,firstName,lastName,city]);
