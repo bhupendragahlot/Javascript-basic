20e65f52a7e163ad4971a48a0524725d2c5bef35
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-05-06 17:54:17 +0000

    datatypes

--- basics/2_dataType.js
+++ basics/2_dataType.js
@@ -21,3 +21,27 @@ console.log(age)
 // objects:
 
 
+// Array,Object,function
+
+//Array:
+let students = ["A","B","C","D"];
+
+// Object:
+
+let myObj ={
+    name:"bhupendra",
+    age:22,
+    regNo:12217388
+}
+console.table(myObj)
+console.table(typeof myObj)
+
+// Function:
+let myFun = function(){
+    console.log("this is function");
+}
+
+myFun();
+console.log( typeof myFun);
+
+
