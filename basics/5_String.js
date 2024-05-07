4b19e0a5d240ad9b639ecf0aa5892ee6b04a8ebf
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-05-07 19:06:24 +0000

    string

--- basics/5_String.js
+++ basics/5_String.js
@@ -6,13 +6,18 @@ console.log(`hello ${firstName} ${lastName}`);
 
 // second way of declaration strings
 
-const hero= new String(`londa`);
+const hero= new String(` bhupendra-gahlot`);
 console.log(hero[0]);
 console.log(hero.length);
 console.log(hero.charAt(2));
 console.log(hero.indexOf('d'));
 console.log(hero.toLocaleUpperCase());
 
-let newString = hero.substring(0,3);
-console.log(newString);
-let otherSting = hero.slice
+console.log('this trim fun: '+hero.trim())
+console.log(hero.replace('-', '_')); 
+console.log(hero.includes("pen"));
+
+// let newString = hero.substring(0,3);
+// console.log(newString);
+// let otherSting = hero.slice(-2,8);
+// console.log(otherSting);
