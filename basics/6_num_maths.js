6aa10acd1c9663af19ec6ed8979100b1853dad00
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-05-11 19:34:41 +0000

    math

--- basics/6_num_maths.js
+++ basics/6_num_maths.js
@@ -5,3 +5,13 @@ console.log(typeof(num));
 const num2=new Number(5000);
 console.log(num2);
 console.log(typeof(num2));
+console.log(num2.toString().length);
+console.log(num2.toFixed(2));
+
+var num3 = 123.8966;
+console.log(num2.toPrecision(2));
+console.log(num2.toPrecision(3));
+
+const hund = 100000000000;
+console.log(hund.toLocaleString()); // for converting us standard 
+console.log(hund.toLocaleString('en-IN')); // for converting Indian standards 
