9b7e44fb6e4345b451c2ada650a2867768de39b0
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-05-30 13:34:35 +0000

    arr

---
+++ basics/8_array.js
@@ -0,0 +1,34 @@
+//array
+
+const myarr = [0,1,2,3,4,5]
+// const student = ["A","b","c"]
+
+// const myarr2 = new Array(1,2,3,4)
+// console.log(myarr[1]);
+
+// //Array methods
+// myarr.push(66);
+// myarr.push("bhupendra");
+// myarr.pop() // remove value 
+// // myarr.unshift(11) // data store 1st indexed 
+// myarr.shift() // remove 0 index value data
+
+// console.log(myarr.includes(5));
+// console.log(myarr.indexOf(5));
+
+
+// const newarr  = myarr.join()
+// console.log(myarr);
+// console.log(newarr);
+
+
+// slice , spice
+console.log("A",myarr);
+
+const myn1 = myarr.slice(1,3)
+console.log("slice", myn1);
+
+console.log("B",myarr);
+
+const myn2 = myarr.splice(1,3)
+console.log(myn2);
