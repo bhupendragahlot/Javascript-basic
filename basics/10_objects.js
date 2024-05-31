c0f1b77feb56ba1c0ddbfe82699517f6574eabc0
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-05-31 12:52:55 +0000

    obj

--- basics/10_objects.js
+++ basics/10_objects.js
@@ -1,25 +1,76 @@
-//singleton
-// Object.create
+// //singleton
+// // Object.create
 
-//object literals
+// //object literals
 
-const mysyb= Symbol("key1");
+// const mysyb= Symbol("key1");
 
-const jsuser ={
-//  key : "values";
-    name:"bhupendra",
-    [mysyb]: "newkey1",
-    age:22,
-    location:"Kota",
-    email:"gahlot@gmil.com",
-    isLOgIn : false,
-    lastLogIn : ["Monday","Saturday"]
+// const jsuser ={
+// //  key : "values";
+//     name:"bhupendra",
+//     [mysyb]: "newkey1",
+//     age:22,
+//     location:"Kota",
+//     email:"gahlot@gmil.com",
+//     isLOgIn : false,
+//     lastLogIn : ["Monday","Saturday"]
+// }
+
+// console.log(jsuser.email);
+// console.log(jsuser["email"])
+// console.log(jsuser[mysyb])
+
+// jsuser.name = "bhupendragahlot";
+
+// console.log(jsuser["name"]);
+
+//////////////////////////////////////////////////////
+
+// const instaUser = new Object() // singleton object
+const instaUser = {} // same but not singleton objects
+
+instaUser.id = "123a";
+instaUser.name = "bhupendra";
+instaUser.islogin = false
+
+// console.log(instaUser);
+
+const newuser = {
+    email: "gahlot@google.com",
+    fullname: {
+        userfullname: {
+            firstname: "bhupendra",
+            lastname: "gahlot"
+        }
+    }
 }
+// console.log(newuser.fullname);
+// console.log(newuser.fullname.userfullname);
+// console.log(newuser.fullname.userfullname.firstname);
 
-console.log(jsuser.email);
-console.log(jsuser["email"])
-console.log(jsuser[mysyb])
 
-jsuser.name = "bhupendragahlot";
+const obj1 = {1: "a",2: "b"}
+const obj2 = {3: "c",4: "d"}
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign(obj1,obj2);
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
 
-console.log(jsuser["name"]);
+const users =[  // in array objects
+    {
+        id: 1,
+        name: "bhupendra"
+    },
+    {
+        id: 2,
+        name: "shiv"
+    },
+]
+
+console.log(users[1].name);
+
+console.log(Object.keys(instaUser));
+console.log(Object.values(instaUser));
+console.log(Object.entries(instaUser));
+
+console.log(instaUser.hasOwnProperty('name')); // for find keys
