1fbcff0a6979b323cf35b149550f25a0b77b1c7d
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-06-03 12:45:49 +0000

    arr

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -13,7 +13,33 @@ const user = {
 // console.log(this);
 
 
-function chai() {
-console.log(this);    
-}
-chai();
+// function chai() {
+//     let username = "bhupendra";
+// console.log(this.username);    
+// }
+// chai();
+
+
+// const  chai2 = function(){
+//     let username = "bhupendra";
+//     console.log(this.username); 
+// }
+// chai2();
+
+// const chai3 =() =>{
+//     let username = "bhupendra";
+//     console.log(`this is arrow function ${this.username}`); 
+// }
+// chai3();
+
+// const addtwo =(num1,num2) =>{
+//     return num1 + num2;
+// }
+// console.log(addtwo(7, 2));
+
+// const addnum =(num1,num2) => num1 + num2;
+// console.log(addnum(7,2));
+
+const addnum2 =(num1,num2) =>({username:"bhupendra"}) 
+console.log(addnum2(7,2));
+
