ca3e817be3adb4b649cbe9e64c4c5c21122a62e8
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-06-05 10:48:23 +0000

    proj

--- project/1-colorChanger/color_button.js
+++ project/1-colorChanger/color_button.js
@@ -1,38 +1,24 @@
 
-// const buttons = document.querySelectorAll('.button');
-// const body = document.querySelector('body');
-
-// buttons.forEach( function (button) {
-//         console.log(button);
-//         button.addEventListener('click', function(e){
-//             console.log(e);
-//             console.log(e.target);
-//             if(e.target.id === 'gray'){
-//                 body.style.backgroundColor =e.target.id;
-//             }
-//             if(e.target.id === 'white'){
-//                 body.style.backgroundColor= e.target.id;
-//             }
-//             if(e.target.id === 'blue'){
-//                 body.style.backgroundColor= e.target.id;
-//             }
-//             if(e.target.id === 'yellow'){
-//                 body.style.backgroundColor= e.target.id;
-//             }
-//         })
-// });
-
-
-const btn = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
-btn.forEach( function(button){
-    console.log(button);
-    button,addEventListener('click', function(e){
-        console.log(e);
-        console.log(e.target);
-        if (e.target.id === 'gray') {
-            body.style.backgroundColor =e.target.id;
-        }
-    })
-})
+buttons.forEach( function (button) {
+        console.log(button);
+        button.addEventListener('click', function(e){
+            console.log(e);
+            console.log(e.target);
+            if(e.target.id === 'gray'){
+                body.style.backgroundColor =e.target.id;
+            }
+            if(e.target.id === 'white'){
+                body.style.backgroundColor= e.target.id;
+            }
+            if(e.target.id === 'blue'){
+                body.style.backgroundColor= e.target.id;
+            }
+            if(e.target.id === 'yellow'){
+                body.style.backgroundColor= e.target.id;
+            }
+        })
+});
+
