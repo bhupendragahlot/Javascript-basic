1f5c20df5ee7bd1e234e75e139169dd269983c64
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-06-05 10:35:02 +0000

    proj

--- project/1-colorChanger/color_button.js
+++ project/1-colorChanger/color_button.js
@@ -1,25 +1,38 @@
 
-const buttons = document.querySelectorAll('.button');
+// const buttons = document.querySelectorAll('.button');
+// const body = document.querySelector('body');
+
+// buttons.forEach( function (button) {
+//         console.log(button);
+//         button.addEventListener('click', function(e){
+//             console.log(e);
+//             console.log(e.target);
+//             if(e.target.id === 'gray'){
+//                 body.style.backgroundColor =e.target.id;
+//             }
+//             if(e.target.id === 'white'){
+//                 body.style.backgroundColor= e.target.id;
+//             }
+//             if(e.target.id === 'blue'){
+//                 body.style.backgroundColor= e.target.id;
+//             }
+//             if(e.target.id === 'yellow'){
+//                 body.style.backgroundColor= e.target.id;
+//             }
+//         })
+// });
+
+
+const btn = document.querySelectorAll('button');
 const body = document.querySelector('body');
 
-buttons.forEach( function (button) {
-        console.log(button);
-        button.addEventListener('click', function(e){
-            console.log(e);
-            console.log(e.target);
-            if(e.target.id === 'gray'){
-                body.style.backgroundColor =e.target.id;
-            }
-            if(e.target.id === 'white'){
-                body.style.backgroundColor= e.target.id;
-            }
-            if(e.target.id === 'blue'){
-                body.style.backgroundColor= e.target.id;
-            }
-            if(e.target.id === 'yellow'){
-                body.style.backgroundColor= e.target.id;
-            }
-        })
-});
-
-
+btn.forEach( function(button){
+    console.log(button);
+    button,addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === 'gray') {
+            body.style.backgroundColor =e.target.id;
+        }
+    })
+})
