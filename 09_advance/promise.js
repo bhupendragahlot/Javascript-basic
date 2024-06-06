8c073fe265c9777bdcee118fc09615f1734e9e36
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-06-06 14:54:25 +0000

    promis

--- 09_advance/promise.js
+++ 09_advance/promise.js
@@ -1,36 +1,59 @@
-const promiseOne = new Promise(function(resolve, reject){
-        //do an asunc task
-        //DB calls, cryptography, network 
+const promiseOne = new Promise(function (resolve, reject) {
+    //do an asunc task
+    //DB calls, cryptography, network 
 
-        setTimeout(function(){
-            console.log('async task is complite');
-            resolve()
-        },1000)
-    });
+    setTimeout(function () {
+        console.log('async task is complite');
+        resolve()
+    }, 1000)
+});
 
 
-promiseOne.then(function(){
+promiseOne.then(function () {
     console.log("promise consumed");
 })
 
-new Promise(function (resolve,reject) {
-setTimeout(function(){
-    console.log("async task 2");
-    resolve()
-},1000)
+new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log("async task 2");
+        resolve()
+    }, 1000)
 
-}).then(function(){
+}).then(function () {
     console.log("async 2 resolved");
 })
 
 
-const PromiseThree= new Promise(function (resolve,reject) {
-    setTimeout(function(){
-        resolve({username:"bhupendra", email:"bhupendra@gamil.com"})
-    },1000)
+const PromiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ username: "bhupendra", email: "bhupendra@gamil.com" })
+    }, 1000)
 })
 
-PromiseThree.then(function(user){
+PromiseThree.then(function (user) {
     console.log(user);
+})
+
+
+
+const PromiseFour = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true;
+        if (!error) {
+            resolve({ username: "bhupendra", password: "123" })
+        }
+        else {
+            reject('ERROR:Something went wrong')
+        }
+    }, 1000)
+})
+
+PromiseFour.then((user) => {
+    console.log(user);
+    return user.username;
+}).then((username)=>{
+console.log(username);
+}).catch(function(error){
+    console.log(error);
 })
 
