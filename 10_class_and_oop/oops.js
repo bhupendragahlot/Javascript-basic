55bc152033e655e2de6d382355cf64f9f2b8f159
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-06-07 14:46:21 +0000

    oops

--- 10_class_and_oop/oops.js
+++ 10_class_and_oop/oops.js
@@ -1,13 +1,29 @@
-const user= {
-    username :"bhupendra",
-    loincount: 8,
-    signin:true,
+// const user= {
+//     username :"bhupendra",
+//     loincount: 8,
+//     signin:true,
 
-    getUserDetails: function() {
-        // console.log("got user details form database");
-        console.log(`Username: ${this.username}`);
+//     getUserDetails: function() {
+//         // console.log("got user details form database");
+//         console.log(`Username: ${this.username}`);
+//     }
+// }
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+function User(username,loincount,islogin){
+    this.username= username;
+    this.loincount = loincount;
+    this.islogin = islogin;
+    this.get=function(){
+        console.log(`welcome ${this.username}`);
     }
+    // return this
 }
 
-console.log(user,username);
-console.log(user.getUserDetails());
+const userone=new User("bhupendra",10,true);
+const usertwo=new User("gahlot",15,true);
+console.log(userone.constructor);
+// console.log(usertwo);
+
